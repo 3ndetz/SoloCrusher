a26ffcdc922b188d11ea066ad1805c3becaf27bf
KolyaNotTheHater <jayrawrr3@gmail.com>
2021-06-20 13:10:55 +0700

    V1.5.1 add vers and Lexa

--- AutoTyper/GetUniqueId.cs
+++ AutoTyper/GetUniqueId.cs
@@ -26,6 +26,8 @@ namespace AutoTyper
             "fdsgf",
             "F3-D9-54-2C-0A-2D-3B-5E-78-89-2B-58-C0-B6-02-A4-7D-16-8F-BC-C6-C5-B4-97-6E-99-9B-57-9F-B0-06-25",
             "35-03-F1-26-3D-63-10-BE-39-00-30-3F-EC-8A-35-A7-27-8A-88-8D-BF-53-A3-7B-FD-4B-2F-8A-EE-2B-9F-9C",
+            "A6-40-96-ED-AB-C3-F0-21-A4-C4-EF-A5-48-C4-3E-45-A4-BC-A1-CE-EF-65-7A-F2-3A-C0-D5-56-73-0C-28-E6",
+            "51-76-4A-4A-59-24-F5-65-3F-17-B9-C5-8E-DB-9D-0B-54-B9-F4-C8-B2-E5-80-9E-70-BB-BB-40-7C-82-4D-4B",
         };
         public static string UniqueHardwaeId
         {
